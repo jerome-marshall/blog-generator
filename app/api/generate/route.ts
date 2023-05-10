c6f5ad76362a579bc4a4c2733a2b8ed1078c3f04
Jerome Marshall <jeromemarshall0@gmail.com>
2023-05-10 20:17:51 +0530

    ⏫ UPDATE: max tokens

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -27,7 +27,7 @@ export async function POST(req: NextRequest) {
     temperature: 0,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 2000,
+    max_tokens: 4000,
     stream: true,
     n: 1,
   };
